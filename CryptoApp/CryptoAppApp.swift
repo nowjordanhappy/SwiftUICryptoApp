159682e286d78f09dd7206eeb28068b50c2a9827
Jordan Rojas <rojas.curl@gmail.com>
2024-03-08 19:56:37 -0500

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Jordan Rojas Alarcon on 8/03/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
